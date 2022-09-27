b8ce63d2fe4dd9a6f543a655a61303cae228b41b
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-27 11:58:28 +0200

    Anti-Fraud System - Stage 2: User fix

--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/User.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/User.java
@@ -4,18 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
