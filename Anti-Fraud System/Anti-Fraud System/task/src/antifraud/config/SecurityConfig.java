d19e4aba3c42a7b53f9cbeae0d7eb507632522d6
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-27 11:53:10 +0200

    Anti-Fraud System - Stage 2: Password encoder removed from SecurityConfig

--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -41,10 +39,5 @@ public class SecurityConfig {
     AuthenticationEntryPoint restAuthenticationEntryPoint() {
         return (request, response, authException) -> response.sendError(
                 HttpStatus.UNAUTHORIZED.value(), authException.getMessage());
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
