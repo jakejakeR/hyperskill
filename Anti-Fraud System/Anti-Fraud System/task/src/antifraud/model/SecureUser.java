441fecfb69026c818efa96314a24689ad8352bc2
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-27 10:29:10 +0200

    Anti-Fraud System - Stage 2: SecureUser added

---
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/SecureUser.java
@@ -0,0 +1,59 @@
+package antifraud.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+
+public class SecureUser implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = -6690946490872875352L;
+
+    private final User user;
+
+    public SecureUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/User.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/User.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Getter
@@ -16,7 +18,14 @@ public class User {
     @GeneratedValue
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
+
+    @NotEmpty
     private String name;
+
+    @Column(unique = true)
     private String username;
+
+    @NotEmpty
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
