75a173b64bc1752d79adcd6166ff077fa036adc9
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-29 15:15:50 +0200

    Anti-Fraud System - Stage 4: Changed id generation type for entities

--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/ip/SuspiciousIp.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/ip/SuspiciousIp.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
@@ -18,7 +19,7 @@ import javax.validation.constraints.NotBlank;
 public class SuspiciousIp {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/user/User.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/user/User.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
