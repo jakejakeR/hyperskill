0fec347093b437606c5f2e0ce4419ba7d7eb5647
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-30 16:19:40 +0200

    Anti-Fraud System - Stage 5: Enhanced TransactionRequest with required fields

--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/transaction/TransactionRequest.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/transaction/TransactionRequest.java
@@ -1,15 +1,29 @@
 package antifraud.model.transaction;
 
+import antifraud.validation.AvailableRegion;
 import antifraud.validation.Ipv4;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
 
-@Data
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class TransactionRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
 
     @NotNull
     @Positive
@@ -22,4 +36,10 @@ public class TransactionRequest {
     @Ipv4
     @NotBlank
     private String ip;
+
+    @Enumerated
+    @AvailableRegion(enumClass = Region.class)
+    private Region region;
+
+    private LocalDateTime date;
 }
