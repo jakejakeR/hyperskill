096008e29196f2354e2d5595e444924ff0e79d15
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-03 12:40:20 +0200

    Anti-Fraud System - Stage 5: Added find method in transaction repo

--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/transaction/TransactionRequest.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/model/transaction/TransactionRequest.java
@@ -2,8 +2,10 @@ package antifraud.model.transaction;
 
 import antifraud.validation.AvailableRegion;
 import antifraud.validation.Ipv4;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.CreditCardNumber;
@@ -18,28 +20,25 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class TransactionRequest {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
-
     @NotNull
     @Positive
     private Long amount;
-
     @NotBlank
     @CreditCardNumber
     private String number;
-
     @Ipv4
     @NotBlank
     private String ip;
-
     @Enumerated
     @AvailableRegion(enumClass = Region.class)
     private Region region;
-
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime date;
 }
--- Anti-Fraud System/Anti-Fraud System/task/src/antifraud/repository/TransactionRepository.java
+++ Anti-Fraud System/Anti-Fraud System/task/src/antifraud/repository/TransactionRepository.java
@@ -3,5 +3,9 @@ package antifraud.repository;
 import antifraud.model.transaction.TransactionRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<TransactionRequest, Long> {
+    List<TransactionRequest> findByNumberAndDateBetween(String number, LocalDateTime dateStart, LocalDateTime dateEnd);
 }
