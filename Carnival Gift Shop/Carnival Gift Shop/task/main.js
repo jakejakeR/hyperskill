994c8d3859f01fc0e5e30e0c046620c053ae9b3d
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-08 12:46:54 +0200

    Stage 3: What visitor wants?

--- Carnival Gift Shop/Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/Carnival Gift Shop/task/main.js
@@ -1,4 +1,6 @@
-const welcomeMessage = () => {
+const input = require('sync-input');
+
+const printWelcomeMessage = () => {
     console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!
 Hello friend! Thank you for visiting the carnival!`)
 };
@@ -23,5 +25,71 @@ const showGifts = () => {
     }
 };
 
-welcomeMessage();
+const printMenu = () => {
+    console.log(`
+What do you want to do?
+1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts`)
+};
+
+let tickets = 100;
+
+const isValid = (input) => {
+    if (Number.isNaN(input)) {
+        console.log("Wrong input.");
+        return false;
+    }
+    return true;
+}
+
+const buyGift = () => {
+    const giftId = Number(input(("Enter the number of the gift you want to get: ")));
+
+    if (!isValid(giftId)) return;
+    if (giftId in gifts) {
+        console.log(`Here you go, one ${gifts[giftId].name}!`);
+        tickets -= gifts[giftId].price;
+        checkTickets();
+    } else {
+        console.log(`There's no gift with id: ${giftId}.`);
+    }
+}
+
+const addTickets = () => {
+    const toAdd = Number(input("Enter the ticket amount: "));
+    if (!isValid(toAdd)) return;
+    tickets += toAdd;
+    checkTickets();
+};
+
+const checkTickets = () => {
+    console.log(`Total tickets: ${tickets}
+Have a nice day!`);
+};
+
+const handleChoice = () => {
+    printMenu();
+
+    const choice = input();
+    switch (choice) {
+        case "1":
+            buyGift();
+            break;
+        case "2":
+            addTickets();
+            break;
+        case "3":
+            checkTickets();
+            break;
+        case "4":
+            showGifts();
+            console.log("Have a nice day!");
+            break;
+        default:
+            console.log("Not available option.");
+            break;
+    }
+}
+
+printWelcomeMessage();
 showGifts();
+handleChoice();
