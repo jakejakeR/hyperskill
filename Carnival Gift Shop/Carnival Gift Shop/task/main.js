50d5c6fe7f6665c3f9c6b5978053f62680b40016
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-08 11:31:48 +0200

    Stage 2: How much?

--- Carnival Gift Shop/Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/Carnival Gift Shop/task/main.js
@@ -4,23 +4,23 @@ Hello friend! Thank you for visiting the carnival!`)
 };
 
 gifts = {
-    1: {name: "Teddy Bear"},
-    2: {name: "Big Red Ball"},
-    3: {name: "Huge Bear"},
-    4: {name: "Candy"},
-    5: {name: "Stuffed Tiger"},
-    6: {name: "Stuffed Dragon"},
-    7: {name: "Skateboard"},
-    8: {name: "Toy Car"},
-    9: {name: "Basketball"},
-    10: {name: "Scary Mask"}
+    1: {name: "Teddy Bear", price: 10},
+    2: {name: "Big Red Ball", price: 5},
+    3: {name: "Huge Bear", price: 50},
+    4: {name: "Candy", price: 8},
+    5: {name: "Stuffed Tiger", price: 15},
+    6: {name: "Stuffed Dragon", price: 30},
+    7: {name: "Skateboard", price: 100},
+    8: {name: "Toy Car", price: 25},
+    9: {name: "Basketball", price: 20},
+    10: {name: "Scary Mask", price: 75}
 };
 
 const showGifts = () => {
     console.log("Here's the list of gifts:\n");
-    Object.values(gifts).forEach(
-        gift => console.log(gift.name)
-    );
+    for (const [id, data] of Object.entries(gifts)) {
+        console.log(`${id}- ${data.name}, Cost: ${data.price} tickets`)
+    }
 };
 
 welcomeMessage();
